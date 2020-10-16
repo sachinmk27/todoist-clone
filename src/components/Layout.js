7ea3a64370d99a8af28429f76787b1835465b7c1
Sachin M K <sachinmk27@gmail.com>
2020-10-16 10:35:49 +0530

    Fix: add children prop to render in Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@chakra-ui/core";
 
 const Layout = ({ children }) => {
-  return <Box w={[4 / 5]}></Box>;
+  return <Box w={[4 / 5]}>{children}</Box>;
 };
 
 export default Layout;
